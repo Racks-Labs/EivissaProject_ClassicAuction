4c4e5eb80312713128b87d2d16ca19bcf4c94436
Jommys <jserrano.gonzalez.moreno@gmail.com>
2022-07-06 20:23:45 +0200

    segfault solved in addBidder

--- test/Auction2.js
+++ test/Auction2.js
@@ -22,7 +22,7 @@ describe("Auction2 Test", async function () {
 		eivissaContract = await EivissaContract.deploy("uri", mrc.address, usdc.address, [10, 6, 3], [100, 200, 300]);
 
 		// Generate a sale contract
-		await eivissaContract.newSale([5, 3, 2], "sale1");
+		await eivissaContract.newSale([2, 2, 1], "sale1");
 		const saleAddress = await eivissaContract.sales(0);
 		const Sale = await ethers.getContractFactory("Sale");
 		saleContract = Sale.attach(saleAddress);
@@ -47,11 +47,75 @@ describe("Auction2 Test", async function () {
 
 	describe("exceptions", () => {
 		it("should revert if user not whitelisted", async () => {
-			await usdc.connect(acc2).approve(auctionContract.address, 100);
 			await expect(saleContract.buy(0)).to.be.revertedWith("whitelistErr");
 			await expect(auctionContract.connect(acc2).bid(0, 100)).to.be.revertedWith("whitelistErr");
-			//const transaction = await auctionContract.connect(acc2).bid(0, 100);
-			//console.log(transaction);
+			await auctionContract.addToWhitelist([acc1.address, acc2.address]);
+			await saleContract.addToWhitelist([acc2.address]);
+			await expect(auctionContract.connect(acc2).bid(0, 100)).to.be.revertedWith("ERC20: insufficient allowance");
+			await expect(saleContract.connect(acc2).buy(0)).to.be.revertedWith("ERC20: insufficient allowance");
+		});
+	});
+
+	describe("bid logic", () => {
+		it("should bid correctly", async () => {
+			await auctionContract.addToWhitelist([acc1.address, acc2.address]);
+			await usdc.connect(acc1).approve(auctionContract.address, 600);
+
+			//await auctionContract.connect(acc1).bid(0, 100);
+			await auctionContract.connect(acc1).bid(2, 350);
+		});
+	});
+
+	describe("Sale", () => {
+		it("should buy all nfts of a sale", async () => {
+			// mint usdc for left addresses
+			await usdc.connect(addrs[0]).mintMore();
+			await usdc.connect(addrs[1]).mintMore();
+			await mrc.connect(addrs[0]).mint(1);
+			await mrc.connect(addrs[1]).mint(1);
+
+			// Add to whitelist test addresses
+			await saleContract.addToWhitelist([
+				deployer.address,
+				acc1.address,
+				acc2.address,
+				addrs[0].address,
+				addrs[1].address,
+			]);
+
+			await usdc.approve(saleContract.address, 100);
+			await usdc.connect(acc1).approve(saleContract.address, 100);
+			await usdc.connect(acc2).approve(saleContract.address, 200);
+			await usdc.connect(addrs[0]).approve(saleContract.address, 200);
+			await usdc.connect(addrs[1]).approve(saleContract.address, 300);
+
+			//mint nfts
+			await saleContract.buy(0);
+			await saleContract.connect(acc1).buy(0);
+			await saleContract.connect(acc2).buy(1);
+			await saleContract.connect(addrs[0]).buy(1);
+			await saleContract.connect(addrs[1]).buy(2);
+
+			// revert if user already minted
+			await expect(saleContract.buy(0)).to.be.reverted;
+
+			// check minter balances
+			expect(await eivissaContract.balanceOf(deployer.address, 0)).to.be.equal(1);
+			expect(await eivissaContract.balanceOf(acc1.address, 0)).to.be.equal(1);
+			expect(await eivissaContract.balanceOf(acc2.address, 1)).to.be.equal(1);
+			expect(await eivissaContract.balanceOf(addrs[0].address, 1)).to.be.equal(1);
+			expect(await eivissaContract.balanceOf(addrs[1].address, 2)).to.be.equal(1);
+
+			// check usdc has been transfered to eivissa
+			expect(await usdc.balanceOf(eivissaContract.address)).to.be.equal(900);
+
+			// check withdraw of the funds
+			const balOfDeployer = Number(await usdc.balanceOf(deployer.address));
+			const balOfEivissa = Number(await usdc.balanceOf(eivissaContract.address));
+			await eivissaContract.withdraw();
+			const balAfterWithdraw = Number(await usdc.balanceOf(deployer.address));
+
+			expect(balAfterWithdraw).to.be.equal(balOfDeployer + balOfEivissa);
 		});
 	});
 });
